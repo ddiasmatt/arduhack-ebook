8985d695c822df827917964ece47310a6def7a98
Mateus Dias <ma.domingues.dias@gmail.com>
2020-05-04 10:15:15 -0300

    added exe_10

--- exe_10/exe_10.ino
+++ exe_10/exe_10.ino
@@ -1,7 +1,17 @@
+#define pin 13
+
 void setup() {
-  Serial.begin(9600); // inicia a Serial
+	Serial.begin(9600);
+  pinMode(pin, OUTPUT);
+  for(int i = 50; i >= 0; i--){
+  	Serial.print("O valor da contagem e: ");
+    Serial.println(i);
+  }
+  Serial.println("Contagem finalizada");
+  digitalWrite(pin, HIGH);
+
 }
 
-void loop() { 
+void loop()  {
 
 }
